2b45232e06922ba28828d3f24a6a80240fb0db32
Yamin Mahdi <yamin_khan@asia.com>
2023-11-27 09:45:47 +0600

    init

--- app/src/main/java/com/rfl/html2pdf/MainActivity.kt
+++ app/src/main/java/com/rfl/html2pdf/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         PDFHelper.apply {
             title= "Due-Invoice"
             orderId ="OJF4638"
